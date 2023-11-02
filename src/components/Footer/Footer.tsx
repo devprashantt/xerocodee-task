0ccd6299b326aaf3488c16d110226d9834c0f7cc
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-11-02 20:10:40 +0530

    all section done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,125 @@
+import { images } from '../../constants'
+import styles from './Footer.module.scss'
+
+const linkData = [
+    {
+        title: 'Home',
+        link: '/'
+    },
+    {
+        title: 'About',
+        link: '/about'
+    },
+    {
+        title: 'Services',
+        link: '/services'
+    },
+    {
+        title: 'Contact',
+        link: '/contact'
+    },
+]
+
+const servicesData = [
+    {
+        title: 'Cloud Hosting',
+        link: '/cloud-hosting'
+    },
+    {
+        title: 'Dedicated Hosting',
+        link: '/dedicated-hosting'
+    },
+    {
+        title: 'VPS Hosting',
+        link: '/vps-hosting'
+    },
+    {
+        title: 'Domain Names',
+        link: '/domain-names'
+    },
+    {
+        title: 'Website Builder',
+        link: '/website-builder'
+    },
+    {
+        title: 'Email Hosting',
+        link: '/email-hosting'
+    },
+]
+
+const otherData = [
+    {
+        title: 'Blog',
+        link: '/blog'
+    },
+    {
+        title: 'Knowledgebase',
+        link: '/knowledgebase'
+    },
+    {
+        title: 'Status',
+        link: '/status'
+    },
+    {
+        title: 'Affiliates',
+        link: '/affiliates'
+    },
+    {
+        title: 'Contact Us',
+        link: '/contact-us'
+    },
+
+]
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className={styles.footer}>
+            <div className={styles.links}>
+                <div className={styles.section}>
+                    <img src={images.logo} alt="" />
+                    <p>You get just what you need to run your cloud
+                        workloads--no more, no less.
+                        Deploy from our single pane of glass,
+                        manage them with ease and scale
+                        up as fast as your workload grows</p>
+                    <div className={styles.icons}>
+                        <div className={styles.icon}></div>
+                        <div className={styles.icon}></div>
+                        <div className={styles.icon}></div>
+                        <div className={styles.icon}></div>
+                    </div>
+                </div>
+                <div className={styles.section}>
+                    <h3>Our Links</h3>
+                    {
+                        linkData.map((data, index) => (
+                            <a key={index} href={data.link}>{data.title}</a>
+                        ))
+                    }
+                </div>
+                <div className={styles.section}>
+                    <h3>Our Services</h3>
+                    {
+                        servicesData.map((data, index) => (
+                            <a key={index} href={data.link}>{data.title}</a>
+                        ))
+                    }
+                </div>
+                <div className={styles.section}>
+                    <h3>Other Links</h3>
+                    {
+                        otherData.map((data, index) => (
+                            <a key={index} href={data.link}>{data.title}</a>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className={styles.bottom}>
+                <p className={styles.term}>Terms & Condition | Privacy Policy </p>
+                <p className={styles.copyright}>Copyright © 2023 EXO-CODE TECHNOLOGIES | All rights reserved</p>
+            </div>
+        </footer>
     )
 }
 
